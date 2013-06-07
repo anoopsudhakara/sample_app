1e1b867d6992ad3b4332ab68768af68581569ce0
anoopsudhakara <anoop.newbie@invoscape.com>
2013-06-07 16:55:59 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'c3959c987563de14d5af75c6542d4c41bd75ad675434df60f6825bbd39efab9c35fb0926280b045ea9a6fe71b5deddcfa2c1634af26efa1e3f96ac71dd671cf8'
